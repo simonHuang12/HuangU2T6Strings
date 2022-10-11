9107c7a7c1a33497146e203912b748dbd1068e3d
Simon Huang <simonh8@nycstudents.net>
2022-10-11 09:30:50 -0400

    a

--- src/LabRunner.java
+++ src/LabRunner.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class LabRunner {
+public class LabRunner {
+    public static void main(String[] args){
+        System.out.println("Why did the chicken cross the road?\n...\nTo get to the other side of the \"\\\"!");
+    }
 }
